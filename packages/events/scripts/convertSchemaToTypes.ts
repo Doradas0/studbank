eaeb1799c7738368e926112a3c1492b0ddd9ab94
Daniel Malcolm <daniel.malcolm@lego.com>
2023-05-26 14:49:01 +0100

    add first schema with conversion

--- packages/events/scripts/convertSchemaToTypes.ts
+++ packages/events/scripts/convertSchemaToTypes.ts
@@ -1,2 +1,17 @@
-import { Lego_Pay_Request } from "../schemas/payments";
-import { compile } from "json-schema-to-typescript";
+import { compileFromFile } from "json-schema-to-typescript";
+import { writeFileSync } from "fs";
+import path from "path";
+
+const main = async () => {
+  console.log("Generating typescript types from json schema");
+  const ts = await compileFromFile(
+    path.resolve(__dirname, "../schemas/Lego_Pay_Request.json"), {
+      additionalProperties: false,
+    }
+  )
+  writeFileSync(path.resolve(__dirname, "../types/Lego_Pay_Request.d.ts"), ts);
+  console.log("Done");
+}
+
+main();
+
