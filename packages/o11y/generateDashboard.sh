2a068f041b916e04077d4ff66cf282e7b37f9e0c
Daniel Malcolm <daniel.malcolm@lego.com>
2023-09-21 13:52:50 +0100

    add full release script
    script will:
    add annotation to dashboard with current date-time
    publish dashboard via aws cli

---
+++ packages/o11y/generateDashboard.sh
@@ -0,0 +1,3 @@
+aws cloudwatch put-dashboard --profile main \
+  --dashboard-name "test-cli-1" \
+  --dashboard-body file://dashboard.json
--- packages/o11y/index.ts
+++ packages/o11y/index.ts
@@ -1,14 +1,56 @@
 const releaseFileDestination = "./releases.json";
 const releasesFile = Bun.file(releaseFileDestination);
 const releases = await releasesFile.json();
-
 const releasesCount = releases.length;
-
 const newRelease = {
   name: `test${releasesCount + 1}`,
   date: new Date().toISOString(),
 };
-
 const newReleases = [...releases, newRelease];
-
 await Bun.write(releaseFileDestination, JSON.stringify(newReleases));
+
+const dashboardAnnotations = newReleases.map((release) => {
+  return {
+    label: release.name,
+    value: release.date,
+    visible: true,
+  }
+});
+
+const dashboardConfig = {
+  widgets: [
+    {
+      type: "metric",
+      x: 0,
+      y: 0,
+      width: 6,
+      height: 6,
+      properties: {
+        view: "timeSeries",
+        stacked: false,
+        metrics: [
+          [
+            "AWS/Usage",
+            "ResourceCount",
+            "Type",
+            "Resource",
+            "Resource",
+            "TableCount",
+            "Service",
+            "DynamoDB",
+            "Class",
+            "None"
+          ]
+        ],
+        region: "eu-west-1",
+        annotations: {
+          vertical: [
+            ...dashboardAnnotations
+          ]
+        }
+      }
+    }
+  ]
+}
+
+await Bun.write("./dashboard.json", JSON.stringify(dashboardConfig));
